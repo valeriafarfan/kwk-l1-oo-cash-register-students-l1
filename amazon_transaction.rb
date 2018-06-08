4c98a21a9aeda56e70b791d896327de7a2f0554f
valeria farfan <valeria073104@gmail.com>
2018-06-08 23:17:16 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,13 @@
-# Code your cash register here!
+require 'colorize'
+def free_shipping
+  puts "What is the total in your cart?".cyan
+  answer=gets.chomp.to_i
+  if answer >= 50
+    puts "YAY!you get free shipping today".yellow
+  else 
+    puts "Remember next time that if you spend more than 50 you get free shipping".magenta
+  end
+end
+free_shipping
+
+  
